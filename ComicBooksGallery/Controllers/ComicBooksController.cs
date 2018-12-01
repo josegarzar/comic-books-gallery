6d6f88ef82c7d06aba1e3f6475046b9228fdd120
José Luis G <josegarzaramos.com>
2018-12-01 13:37:38 -0600

    Added Comic Books Detail view

--- ComicBooksGallery/Controllers/ComicBooksController.cs
+++ ComicBooksGallery/Controllers/ComicBooksController.cs
@@ -10,10 +10,7 @@ namespace ComicBooksGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday) {
-                return Redirect("/");
-            }
-            return Content("Hello from the comic books controller!");
+            return View();
         }
     }
 }
